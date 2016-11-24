230e2fab2bd85e41e7bc4608f11f9cfd8ebf9db7
just nobody <kingdaro@gmail.com>
2016-11-23 23:33:50 -0500

    wrap init in async function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-require('regenerator-runtime/runtime')
+import 'regenerator-runtime/runtime'
 
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
@@ -6,7 +6,7 @@ import App from './app.vue'
 import * as firebase from 'firebase'
 
 import 'vue-material/dist/vue-material.css'
-import 'global.css'
+import './global.css'
 
 Vue.use(VueMaterial)
 
@@ -15,17 +15,26 @@ Vue.material.theme.register('default', {
   accent: 'cyan'
 })
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCSJF99jCNQQTUOi2EXd9XU_ZgGGYP6U7Y",
-  authDomain: "rpchat-59074.firebaseapp.com",
-  databaseURL: "https://rpchat-59074.firebaseio.com",
-  storageBucket: "rpchat-59074.appspot.com",
-  messagingSenderId: "192547961510"
-})
+const init = async function () {
+  try {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCSJF99jCNQQTUOi2EXd9XU_ZgGGYP6U7Y",
+      authDomain: "rpchat-59074.firebaseapp.com",
+      databaseURL: "https://rpchat-59074.firebaseio.com",
+      storageBucket: "rpchat-59074.appspot.com",
+      messagingSenderId: "192547961510"
+    })
 
-firebase.auth().signInAnonymously()
+    await firebase.auth().signInAnonymously()
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    new Vue({
+      el: '#app',
+      render: h => h(App)
+    })
+  }
+  catch (error) {
+    console.error('could not sign in: ', error)
+  }
+}
+
+init()

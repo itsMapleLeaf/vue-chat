62faeacea56ee1f82a31c78a46a7bc10d3e78243
just nobody <kingdaro@gmail.com>
2016-11-23 20:36:30 -0500

    split off chat input component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
   <div class="container flex-vertical" v-md-theme="'default'">
-    <chat-messages class="chat-messages flex-grow" v-bind="{ messages }"></chat-messages>
-    <form class="chat-input-form flex-fixed flex-row" @submit.stop.prevent="chatSubmit">
-      <md-input-container class="chat-input-container flex-grow">
-        <md-input class="chat-input" placeholder="Say something!" v-model="chatInput"></md-input>
-      </md-input-container>
-      <md-button class="chat-send md-primary" @click="chatSubmit">Send</md-button>
-    </form>
+    <chat-messages class="flex-grow" v-bind="{ messages }"></chat-messages>
+    <chat-input class="flex-fixed" @submit="chatSubmit"></chat-input>
   </div>
 </template>
 
 <script>
 import ChatMessages from './chat-messages.vue'
+import ChatInput from './chat-input.vue'
 
 export default {
   name: 'app',
   components: {
-    ChatMessages
+    ChatMessages,
+    ChatInput,
   },
   data: () => ({
     messages: [],
     chatInput: ''
   }),
   methods: {
-    chatSubmit() {
-      if (this.chatInput.trim() !== '') {
-        this.messages.push({
-          text: this.chatInput,
-          time: new Date().toLocaleTimeString(),
-        })
-      }
-      this.chatInput = ''
+    chatSubmit(text) {
+      this.messages.push({ text, time: new Date().toLocaleTimeString(), })
     }
   }
 }
@@ -65,23 +56,5 @@ export default {
 <style scoped>
 .container {
   height: 100vh;
-}
-
-.chat-input-form {
-  padding: 0 1em;
-  background: white;
-}
-
-.chat-input-container {
-  margin: 0.2em 0;
-  padding: 0;
-}
-
-.chat-input {
-  height: 3em;
-}
-
-.chat-send {
-  /* height: 3em; */
 }
 </style>
